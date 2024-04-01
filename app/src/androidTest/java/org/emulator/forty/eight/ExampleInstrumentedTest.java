31b382e81258edd76dc1218470dbdf2aca43aaba
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2024-04-01 21:22:27 +0200

    rename java package s/org.emulator/eu.luoi/g

--- app/src/androidTest/java/org/emulator/forty/eight/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/emulator/forty/eight/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package org.emulator.forty.eight;
+package eu.luoi.forty.eight;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("org.emulator.forty.eight", appContext.getPackageName());
+        assertEquals("eu.luoi.forty.eight", appContext.getPackageName());
     }
 }
